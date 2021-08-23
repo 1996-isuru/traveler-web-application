6c9b9147e2fb8468aa09fce00412f9456d42470a
1996-isuru <isurusrimalnt@gmail.com>
2021-08-23 12:47:58 +0530

    bug fixed login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   Redirect
+// } from "react-router-dom";
 
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
@@ -9,53 +16,37 @@ import TravelerHome from "./components/traveler/travelerHome";
 import AdminHome from "./components/admin/adminHome";
 
 function App() {
-
   const [token, setToken] = useState(false);
   const [localToken, setLocalToken] = useState(null);
 
   const getToken = () => {
-    // console.log(token);
-    // const localToken = null;
     const tokenn = localStorage.getItem("token");
     setLocalToken(tokenn);
-    // console.log(localToken);
-    if(localToken){
+  if (localToken !== null) {
       setToken(true);
-      // console.log(localToken);
-      // console.log(token);
     }
-  }
+  };
 
   useEffect(() => {
     getToken();
   });
-  
-  // console.log(localToken);
-  
+
   return (
     <div className="App">
       <Router>
-      {localToken ? (
-        <>
-        
-        <Route exact path="/travelerhome" component={TravelerHome} />
-        <Route exact path="/adminhome" component={AdminHome} />
-        <Route path="/" exact component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-       
-        </>
-      ) : (
-        <>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return token ? <Redirect to="/travelerHome" /> : <Redirect to="/" />;
+          }}
+        />
         <Route path="/" exact component={Landing} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/travelerhome" component={TravelerHome} />
         <Route exact path="/adminhome" component={AdminHome} />
-        
-      </>
-      )}
-       </Router>
+      </Router>
     </div>
   );
 }
