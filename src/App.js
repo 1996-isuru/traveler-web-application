5ae4ff593868cbc459f4d168fbbfa0cc86099a47
1996-isuru <isurusrimalnt@gmail.com>
2021-08-24 23:36:16 +0530

    hotel admin page create

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Register from "./components/auth/Register";
 import Landing from "./components/auth/landing";
 import TravelerHome from "./components/traveler/travelerHome";
 import AdminHome from "./components/admin/adminHome";
+import HotelHome from "./components/hotel/hotelHome";
+import hotelAdmin from "./components/hotel/hotelAdmin";
 
 function App() {
   const [token, setToken] = useState(false);
@@ -22,7 +24,7 @@ function App() {
   const getToken = () => {
     const tokenn = localStorage.getItem("token");
     setLocalToken(tokenn);
-  if (localToken !== null) {
+    if (localToken !== null) {
       setToken(true);
     }
   };
@@ -38,7 +40,11 @@ function App() {
           exact
           path="/"
           render={() => {
-            return token ? <Redirect to="/travelerHome" /> : <Redirect to="/" />;
+            return token ? (
+              <Redirect to="/hoteladmin" />
+            ) : (
+              <Redirect to="/" />
+            );
           }}
         />
         <Route path="/" exact component={Landing} />
@@ -46,6 +52,8 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/travelerhome" component={TravelerHome} />
         <Route exact path="/adminhome" component={AdminHome} />
+        <Route exact path="/hotel" component={HotelHome} />
+        <Route exact path="/hoteladmin" component={hotelAdmin} />
       </Router>
     </div>
   );
